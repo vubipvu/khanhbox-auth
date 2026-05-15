2381d158d97e917286d6985cfe85c96c27a3b6e2
VU TRAN SY <145173066+vubipvu@users.noreply.github.com>
2026-05-15 11:07:02 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
-const { SimpleGatekeeper } = require('simple-gatekeeper');
-module.exports = SimpleGatekeeper({
-  clientId: process.env.OAUTH_CLIENT_ID,
-  clientSecret: process.env.OAUTH_CLIENT_SECRET,
+const { createProxy } = require('netlify-cms-proxy-auth');
+
+module.exports = createProxy({
+  config: {
+    client_id: process.env.OAUTH_CLIENT_ID,
+    client_secret: process.env.OAUTH_CLIENT_SECRET
+  }
 });
